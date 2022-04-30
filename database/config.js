6159ed9b8f94eae79bae2966c97c0c3fd85152b4
alex <alex.flores.m@uni.pe>
2022-04-30 10:01:19 -0500

    solve

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize("titulos", "admin", "qwaser12", {
+const db = new Sequelize("usuarios", "postgres", "qwaser12", {
   host: "localhost",
   dialect: "postgres",
   logging: false,
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,11 +1,15 @@
-const upload = require('../libs/storage')
 const { Router } = require("express");
-const { postUser, getUser, getUsers, mostrarImage, updateUser } = require("../controllers/usuarios");
+const {
+  postUser,
+  getUser,
+  getUsers,
+  mostrarImage,
+  updateUser,
+} = require("../controllers/usuarios");
 const router = Router();
 
-router.post("/", upload.single('file') ,postUser);
-router.get("/:id",getUser);
-router.get("/",getUsers);
+router.get("/:id", getUser);
+router.get("/", getUsers);
 router.get("/foto/:id", mostrarImage);
 router.put("/change", updateUser);
 
