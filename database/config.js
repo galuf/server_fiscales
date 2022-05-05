c596f49ee8b5c337aa0f0d6a9fe15ef8151cd483
galuf <alex.flores.m@uni.pe>
2022-05-05 17:41:00 -0500

    DB fixed

--- database/config.js
+++ database/config.js
@@ -1,25 +1,25 @@
 const { Sequelize } = require("sequelize");
 
 // Heroku
-// const db = new Sequelize(
-//   "postgres://jvgzkrwbgrptku:8f9ac3718e466ea3ba2e206f7e42504587fe119247589969b7b848c2978972b9@ec2-3-223-213-207.compute-1.amazonaws.com:5432/d57984dbga9cb8",
-//   {
-//     logging: false,
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     },
-//   }
-// );
+const db = new Sequelize(
+  "postgres://rytygopcfaquuh:e353c987e32aa069e92d9f6f02bb11042fabf8fbea2946524c625795f41bbb40@ec2-3-223-213-207.compute-1.amazonaws.com:5432/db6i2ulsptvsh6",
+  {
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 
 // Local
-const db = new Sequelize("usuarios", "postgres", "qwaser12", {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
+//const db = new Sequelize("usuarios", "postgres", "qwaser12", {
+//  host: "localhost",
+//  dialect: "postgres",
+//  logging: false,
+//});
 
 module.exports = {
   db,
