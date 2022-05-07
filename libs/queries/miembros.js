b1538154bc179c811b57b69842509723f0bae282
Louiso <gransullca.25@gmail.com>
2022-05-07 13:28:52 -0500

    []

--- libs/queries/miembros.js
+++ libs/queries/miembros.js
@@ -22,7 +22,7 @@ const MiembrosQueries = {
         miembro_comite, count(*) conteo_convocatorias from public.miembros_comite_seleccion mcs 
         group by miembro_comite
       ), presuntos_anual_total as (
-        select pr.fullname, pr.civil, pr.penal, pr.adm_ent, pr.adm_pas, pr.adm,
+        select pr.fullname, pr.civil, pr.penal, pr.adm_ent, pr.adm_pas, pr.adm, pr.dni,
         extract(year from ic.fecha_emision) anio_implicado
         from public.presuntos_responsables pr 
         left join public.informes_control ic on pr.num_inform = ic.num_inform
@@ -33,10 +33,11 @@ const MiembrosQueries = {
         sum(adm_ent::int) as adm_ent,
         sum(adm_pas::int) as adm_pas,
         sum(adm::int) as adm,
-        count(*) conteo_acusacion
+        count(*) conteo_acusacion,
+        dni
         from presuntos_anual_total
         where anio_implicado >= 2018
-        group by fullname
+        group by fullname, dni
       ), miembros_sobrecarga_infoacusacion as (
         select *
         from sobrecarga_comite_anual sca
@@ -50,7 +51,8 @@ const MiembrosQueries = {
         coalesce (adm_ent, 0) adm_ent,
         coalesce (adm_pas, 0) adm_pas,
         coalesce (adm, 0) adm,
-        coalesce (conteo_acusacion, 0) conteo_acusacion
+        coalesce (conteo_acusacion, 0) conteo_acusacion,
+        coalesce (dni, '') dni
         from miembros_sobrecarga_infoacusacion
       )
         select * from clean_miembros_infoacusacion cmi
