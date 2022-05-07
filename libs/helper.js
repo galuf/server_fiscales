706821e62a7f7d4cec7d7b65610816deeb4a842f
Louiso <gransullca.25@gmail.com>
2022-05-07 13:53:38 -0500

    [update] soporte a espacios y tildes

--- libs/helper.js
+++ libs/helper.js
@@ -52,8 +52,54 @@ const executePagination = async (query, options) => {
   };
 };
 
+const tilderize = (stringText) => stringText.replace(/[aeiouáéíóúnñ]/gi, (v) => {
+  switch (v) {
+    case 'a':
+    case 'A':
+    case 'á':
+    case 'Á':
+      return '(a|á)'
+    case 'e':
+    case 'é':
+    case 'E':
+    case 'É':
+      return '(e|é)'
+    case 'i':
+    case 'í':
+    case 'I':
+    case 'Í':
+      return '(i|í)'
+    case 'o':
+    case 'ó':
+    case 'O':
+    case 'Ó':
+      return '(o|ó)'
+    case 'u':
+    case 'ú':
+    case 'U':
+    case 'Ú':
+      return '(u|ú)'
+    case 'n':
+    case 'ñ':
+    case 'N':
+    case 'Ñ':
+      return '(n|ñ)'
+    default:
+      return ''
+  }
+})
+
+const escapeRegExp = (string) =>  string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+
+const createSearchRegexp = (text) => {
+  const escapedText = tilderize(escapeRegExp(text))
+
+  return escapedText
+}
+
 module.exports = {
   groupBy,
   executePagination,
   pagination,
+  createSearchRegexp
 };
